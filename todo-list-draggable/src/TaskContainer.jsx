add8958c4f26c7c67bd6dc4759d4cc05b24e8bb0
mohamed montaser <mastrocoding@gmail.com>
2023-08-03 23:37:48 +0300

    change taskcontainer destructure

--- todo-list-draggable/src/TaskContainer.jsx
+++ todo-list-draggable/src/TaskContainer.jsx
@@ -6,7 +6,10 @@ export default function TaskContainer({ type }) {
   const { todos } = useTodos();
   return (
     <div className={`task-container ${type}`}>
-      <h3>Active</h3>
+      {/* <h3>{type}</h3> */}
+      {type === "active" && <h3>Active</h3>}
+      {type === "in-progress" && <h3>In Progress</h3>}
+      {type === "complated" && <h3>Complated</h3>}
       <List type={type}>
         {todos.map((todo) => {
           if (todo.mode !== type) return null;
