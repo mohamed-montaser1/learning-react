67abedf269d4382c81c3f478740171073729229a
mohamed montaser <mastrocoding@gmail.com>
2023-08-04 10:11:36 +0300

    remove todo when double click

--- todo-list-draggable/src/ListElement.jsx
+++ todo-list-draggable/src/ListElement.jsx
@@ -1,11 +1,26 @@
+import useTodos from "./context/TodoContext";
+
 export default function ListElement({ title, id, mode }) {
+  const { todos, setTodos } = useTodos();
+
   const handleDragStart = (e) => {
     console.dir(e.target);
     e.dataTransfer.setData("text/plain", id);
   };
+  const removeItem = () => {
+    let newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+    localStorage.setItem("todos", JSON.stringify(newTodos));
+  };
   return (
     <>
-      <li draggable="true" onDragStart={handleDragStart} id={id} mode={mode}>
+      <li
+        draggable="true"
+        onDragStart={handleDragStart}
+        id={id}
+        mode={mode}
+        onDoubleClick={removeItem}
+      >
         {title}
       </li>
     </>
