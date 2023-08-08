3beae6003e8321bbcf2014839cd2dfbc99b2ff9f
mohamed montaser <mastrocoding@gmail.com>
2023-08-08 16:52:10 +0300

    validate todo list input

--- todo-list-draggable/src/Button.jsx
+++ todo-list-draggable/src/Button.jsx
@@ -3,6 +3,7 @@ import useTodos from "./context/TodoContext";
 export default function Button() {
   let { inputValue, setInputValue, addTodo } = useTodos();
   const handleAddTodo = () => {
+    if (inputValue === "") return alert("Please Enter Value In Input!")
     addTodo(inputValue);
     setInputValue("");
   };
